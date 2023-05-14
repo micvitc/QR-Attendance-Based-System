e01f8c7a57b3ee5db1c84b18a32f2d006a9d208c
Malay-dev <malaykumar2003@gmail.com>
2023-05-14 12:20:34 +0530

    Feat: QR code reader and genrator init

---
+++ app/generate/page.js
@@ -0,0 +1,27 @@
+// generate.js
+"use client";
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
+import styles from "../styles/Home.module.css";
+
+function Generate() {
+  const [qrCodeValue, setQrCodeValue] = useState("");
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.card}>Generate QR</div>
+
+      {qrCodeValue != "" && (
+        <QRCode value={qrCodeValue} className={styles.containerColumn} />
+      )}
+      <input
+        className={styles.card}
+        onChange={(e) => {
+          setQrCodeValue(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default Generate;
