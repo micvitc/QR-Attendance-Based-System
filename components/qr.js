8873cee1ef67bbfc97fd03cc319a97931db870b8
Yalamarthi Vinay <yalamarthi.vinay2021@vitstudent.ac.in>
2023-05-19 15:46:38 +0530

    added qr

--- components/qr.js
+++ components/qr.js
@@ -1,12 +1,39 @@
 "use client"
 
-import { Button, ButtonGroup } from '@chakra-ui/react'
-
+import { Box, Button, ButtonGroup, Heading, Stack } from '@chakra-ui/react'
+import { Center, Square, Circle } from '@chakra-ui/react'
+import Image from 'next/image'
+import { Text } from '@chakra-ui/react'
 
 export default function QR(){
     return (
     <>
-<Button colorScheme='blue'>Button</Button>    
+    <Center  h='100px' className='flex flex-col items-center justify-center h-screen' >
+        <Heading m= {10}>QR scanner</Heading>
+
+        <Image width={250} height={250} src='/sample-qr.png' alt='Dan Abramov' />
+        <Stack spacing={3}>
+
+            <Text fontSize='md'> Name  </Text>
+            
+            <Text m = {10} fontSize='md'> Registration number  </Text>
+
+        </Stack>
+
+        <Stack direction='row' spacing={4} align='center'>
+    
+    
+            <Button m = {5} colorScheme='teal' variant='outline'>
+                Finish
+            </Button>
+            <Button colorScheme='teal' variant='ghost'>
+                Next
+
+            </Button>
+        </Stack>
+
+
+</Center>
 </>
     )
 } 
