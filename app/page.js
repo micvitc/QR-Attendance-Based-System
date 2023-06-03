1e6b52f8feee4175f0f73ad9c2cb949a55b2583b
Malay-dev <malaykumar2003@gmail.com>
2023-06-03 13:02:36 +0530

    Fix: erros login

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
-"use client"
+"use client";
 import {
-
   Flex,
   Box,
   FormControl,
@@ -13,82 +12,85 @@ import {
   Heading,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 
+import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 export default function SimpleCard() {
+  const [showPassword, setshowPassword] = useState(false);
   return (
     <>
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Welcome</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            Login with email<Link color={'blue.400'}></Link> 
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
-              </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Sign in
-              </Button>
-            </Stack>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}>
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Welcome</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              Login with email<Link color={"blue.400"}></Link>
+            </Text>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
- 
-
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}>
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={"blue.400"}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}>
+                  Sign in
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
 
       <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'} textAlign={'center'}>
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}>
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"} textAlign={"center"}>
               Sign up
             </Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
+            <Text fontSize={"lg"} color={"gray.600"}>
               with your Email
             </Text>
           </Stack>
           <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
             p={8}>
             <Stack spacing={4}>
-              <HStack>
+              <Stack>
                 <Box>
                   <FormControl id="firstName" isRequired>
                     <FormLabel>First Name</FormLabel>
@@ -101,7 +103,7 @@ export default function SimpleCard() {
                     <Input type="text" />
                   </FormControl>
                 </Box>
-              </HStack>
+              </Stack>
               <FormControl id="regno" isRequired>
                 <FormLabel>Registration Number</FormLabel>
                 <Input type="regno" />
@@ -113,10 +115,10 @@ export default function SimpleCard() {
               <FormControl id="password" isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
-                  <Input type={showPassword ? 'text' : 'password'} />
-                  <InputRightElement h={'full'}>
+                  <Input type={showPassword ? "text" : "password"} />
+                  <InputRightElement h={"full"}>
                     <Button
-                      variant={'ghost'}
+                      variant={"ghost"}
                       onClick={() =>
                         setShowPassword((showPassword) => !showPassword)
                       }>
@@ -129,24 +131,23 @@ export default function SimpleCard() {
                 <Button
                   loadingText="Submitting"
                   size="lg"
-                  bg={'blue.400'}
-                  color={'white'}
+                  bg={"blue.400"}
+                  color={"white"}
                   _hover={{
-                    bg: 'blue.500',
+                    bg: "blue.500",
                   }}>
                   Sign up
                 </Button>
               </Stack>
               <Stack pt={6}>
-                <Text align={'center'}>
-                  Already a user? <Link color={'blue.400'}>Login</Link>
+                <Text align={"center"}>
+                  Already a user? <Link color={"blue.400"}>Login</Link>
                 </Text>
               </Stack>
             </Stack>
           </Box>
         </Stack>
       </Flex>
-</>
-    );
-  }
-
+    </>
+  );
+}
