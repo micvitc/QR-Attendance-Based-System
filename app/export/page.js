8f898ce2c74c47a48fa61904d268fedda85adc33
Harsh-jn <97253124+Harsh-jn@users.noreply.github.com>
2023-07-05 22:04:56 +0530

    Create page.js
    Importing the csv file

---
+++ app/export/page.js
@@ -0,0 +1,84 @@
+'use client'
+import React, { useState } from "react";
+
+function App() {
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
+
+  const fileReader = new FileReader();
+
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const csvFileToArray = (string) => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const newArray = csvRows.map((i) => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(newArray);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text);
+      };
+
+      fileReader.readAsText(file);
+    }
+  };
+
+  const headerKeys = Object.keys(Object.assign({}, ...array));
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>REACTJS CSV IMPORT EXAMPLE </h1>
+      <form>
+        <input
+          type="file"
+          id="csvFileInput"
+          accept=".csv"
+          onChange={handleOnChange}
+        />
+
+        <button onClick={(e) => handleOnSubmit(e)}>IMPORT CSV</button>
+      </form>
+
+      <br />
+
+      <table>
+        <thead>
+          <tr key="header">
+            {headerKeys.map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item, index) => (
+            <tr key={index}>
+              {Object.values(item).map((val, index) => (
+                <td key={index}>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
