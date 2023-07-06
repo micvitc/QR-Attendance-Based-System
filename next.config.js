d389f1cace959fdeb55741c97e3056ada75b9f02
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-07-06 14:45:06 +0530

    Events added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,51 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config) => {
+        // this will override the experiments
+        config.experiments = { ...config.experiments, topLevelAwait: true };
+        // this will just update topLevelAwait property of config.experiments
+        // config.experiments.topLevelAwait = true 
+        return config;
+      },
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Feature-Policy',
+                        value: "geolocation 'self'; microphone 'self'; camera 'self'",
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: "geolocation=(self), microphone=()",
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://www.google-analytics.com; frame-src 'self' https://www.google.com https://www.youtube.com; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content; manifest-src 'self'; worker-src 'self'; frame-ancestors 'none';",
+                    },
+                ],
+            },
+        ];
+    },
+};
+
 
 module.exports = nextConfig
