675b97c0a41a9d4c6d0e04e22c93cdd1d3731ca7
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-07-09 13:17:56 +0530

    Added attendennce view

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -29,7 +29,7 @@ export default async function Navbar() {
       <nav className="bg-blue-500 border-gray-200">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link href="/" className="flex items-center">
-            <img src="logo.png" className="h-8 mr-3" alt="MIC Logo" />
+            <img src="/logo.png" className="h-8 mr-3" alt="MIC Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
               MIC
             </span>
